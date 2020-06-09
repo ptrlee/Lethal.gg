36bb1a2ba18f383df84dfb1ad6a8a697e910b412
Finn Feighery <peterlee26717@yahoo.com>
2020-06-09 17:09:08 -0400

    temp fix

--- live.js
+++ live.js
@@ -7,11 +7,35 @@ input.addEventListener("keyup", function(event) {
     event.preventDefault();
 
     let player = this.value;
-    let temp = $.getValues(`https://127.0.0.1:2999/liveclientdata/${player}`);
+    let temp = $.getValues(`https://127.0.0.1:2999/liveclientdata/Coolnoob215`);
     console.log(temp);
 
   }
 });
+
+// const axios = require('axios').default;
+
+// var https = require('https');
+// var fs  = require('fs');
+
+// var options = {
+//   hostname: 'example.com',
+//   port: 83,
+//   path: '/v1/api?a=b',
+//   method: 'GET',
+//   key: fs.readFileSync('/path/to/private-key/key.pem'),
+//   cert: fs.readFileSync('/path/to/certificate/client_cert.pem'),  
+//   passphrase: 'password'
+// };
+
+// var req = https.request(options, function(res) {
+// console.log(res.statusCode);
+// res.on('data', function(d) {
+//   process.stdout.write(d);
+//   });
+// });
+
+// req.end()
 
 jQuery.extend({
     getValues: function(url) {
